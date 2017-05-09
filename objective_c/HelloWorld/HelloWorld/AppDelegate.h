f1b92c18eb52cdf73ebc22fdb404585827262509
Joe Cortopassi <joe@joecortopassi.com>
2017-05-08 19:47:15 -0700

    Initial project creation

---
+++ objective_c/HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Joseph Cortopassi on 5/8/17.
+//  Copyright © 2017 Joseph Cortopassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objective_c/HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by Joseph Cortopassi on 5/8/17.
+//  Copyright © 2017 Joseph Cortopassi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
